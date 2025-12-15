776c52c56c64b95254706f30e961af760cd480cb
itworks2018 <itworks.shimt2018@gmail.com>
2025-12-15 15:50:31 +0800

    updated app files v3

--- src/middlewares/errorHandler.js
+++
@@ -1,6 +0,0 @@
-export default function errorHandler(err, req, res, next) {
-  console.error(err)
-  const status = err.status || 500
-  const message = err.message || 'Internal Server Error'
-  res.status(status).json({ error: message })
-}
