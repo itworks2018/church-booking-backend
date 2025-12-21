e74ca32b6a2f84c756a3394e772fad5b5be85c5c
itworks2018 <itworks.shimt2018@gmail.com>
2025-12-22 06:03:22 +0800

    updated backend files

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { getMyProfile, updateMyProfile } from "../controllers/profile.controller.js";
-import { authMiddleware } from "../middleware/authmiddleware.js";
+import { authMiddleware } from "../middleware/auth.middleware.js";
 
 const router = express.Router();
 
