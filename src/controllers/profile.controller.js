98d04cd4de443583ad352f521ecf4c37a8fc347d
itworks2018 <itworks.shimt2018@gmail.com>
2026-01-26 08:17:26 +0800

    updated backend

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -44,3 +44,18 @@ export const updateMyProfile = async (req, res) => {
     res.status(500).json({ error: "Server error" });
   }
 };
+
+// GET /api/profiles
+export const getAllProfiles = async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("users")
+      .select("user_id, full_name, email, contact_number, role");
+
+    if (error) return res.status(400).json({ error: error.message });
+
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+};
